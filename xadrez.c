d02d353dc51967af17d30f93474193eb863a8a07
RafaQATester <rafaelqatester51@gmail.com>
2025-02-27 11:38:36 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -4,12 +4,14 @@ int main(){
     int torre = 1;
     int bispo = 1;
     int rainha = 1;
-/*
+
+//Utilizando for para mover Torre
     for (torre = 1; torre <= 5; torre++)
     {
         printf("Direita\n");
     }
 
+//Utilizando While para mover Bispo
     while (bispo <= 5)
     {
 
@@ -17,7 +19,8 @@ int main(){
         bispo++;
 
     }
-*/
+
+//Utilizando Do-While para mover Rainha
     do {
 
         printf("Esquerda\n");
